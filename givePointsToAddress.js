f19fe0670bd7fee88ad2f03e6b502e81839427ca
Andrei VASILACHE <andrei.vasilache@assist.ro>
2021-12-03 22:16:33 +0200

    Mainnet migration

--- givePointsToAddress.js
+++ givePointsToAddress.js
@@ -3,7 +3,9 @@ const { getInfuraInstance } = require("./getInfuraInstance");
 const { sourceWallet } = require("./secrets");
 
 module.exports.givePointsToAddress = async (db, to, amount) => {
-  const smartContractData = db.collection("smart-contracts").doc("MarketCoins");
+  const smartContractData = db
+    .collection("smart-contracts")
+    .doc("MarketCoins_matic");
   const doc = await smartContractData.get();
   const { abi, address: smartContractAddress } = doc.data();
   const fromWallet = getInfuraInstance(sourceWallet).wallet;
